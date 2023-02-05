40ba798a486579213f82355315807f1eab490336
Herr-Hauptmann <tarik.horoz@gmail.com>
2023-02-05 23:55:01 +0100

    add partners

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,12 +2,14 @@
 import Landing from '../partials/Home/Landing.vue'
 import About from '../partials/Home/About.vue'
 import Info from '../partials/Home/Info.vue'
+import Partners from '../partials/Home/Partners.vue'
 </script>
 
 <template>
 <Landing />
 <About />
 <Info />
+<Partners />
 </template>
 
 <style scoped>
